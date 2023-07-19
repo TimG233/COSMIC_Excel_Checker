0b4612ff9f9de19245cafd79819784d86b654dd3
TimG233 <54853713+TimG233@users.noreply.github.com>
2023-07-19 16:35:33 +0800

    chore: bump version to 0.2.2a

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('./README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name="cosmicexcelchecker",
-    version="0.2.1",
+    version="0.2.1a",
     license="GPL-3.0-only",
     author="TimG233",
     author_email="gaosh0830@gmail.com",
